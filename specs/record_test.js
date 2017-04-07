b485e94f292b356e68a99b5a3724415680283034
Adam <adam8690@hotmail.co.uk>
2017-04-07 11:05:35 +0100

    tested record has all the things

--- specs/record_test.js
+++ specs/record_test.js
@@ -2,9 +2,21 @@ var Record = require('../record')
 var assert = require('assert')
 
 describe('record', function(){
-  it('should have a name', function(){
+  it('should have an artist', function(){
     var record = new Record("David Bowie", "China Girl", "Rock", 300)
     assert.strictEqual("David Bowie", record.artist)
+  })
+    it('should have title', function(){
+    var record = new Record("David Bowie", "China Girl", "Rock", 300)
+    assert.strictEqual("China Girl", record.title)
+  })
+    it('should have a genre', function(){
+    var record = new Record("David Bowie", "China Girl", "Rock", 300)
+    assert.strictEqual("Rock", record.genre)
+  })
+    it('should have a price', function(){
+    var record = new Record("David Bowie", "China Girl", "Rock", 300)
+    assert.strictEqual(300, record.price)
   })
 
 
