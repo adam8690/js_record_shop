f66f44aceb6663bff30e9f962c917ed548709b69
Adam <adam8690@hotmail.co.uk>
2017-04-07 13:50:29 +0100

    can add records to recordstore

--- specs/record_store_test.js
+++ specs/record_store_test.js
@@ -1,23 +1,31 @@
 var RecordStore = require('../record_store')
+var Record = require('../record')
 var assert = require('assert')
 
 describe('record store', function(){
+
+  recordStore = new RecordStore("1up", "Aberdeen");
+  record1 = new Record("David Bowie", "China Girl", "Rock", 300)
+
   it('should have a name', function(){
-    recordStore = new RecordStore("1up", "Aberdeen");
     assert.strictEqual('1up', recordStore.name)
     })  
 
   it('should have a city', function(){
-    recordStore = new RecordStore("1up", "Aberdeen");
     assert.strictEqual('Aberdeen', recordStore.city)
     })  
 
   it('should start with an empty inventory', function(){
-    recordStore = new RecordStore("1up", "Aberdeen");
     assert.deepEqual([], recordStore.inventory)
     })
   it('should start with zero balance', function(){
-    recordStore = new RecordStore("1up", "Aberdeen");
     assert.deepEqual(0, recordStore.balance)
     })
+
+  it('should be able to add records to store inventory', function(){
+    recordStore.addRecord(record1);
+    recordStore.addRecord(record1);
+    assert.deepEqual([record1, record1], recordStore.inventory)
+  })
+
 })
